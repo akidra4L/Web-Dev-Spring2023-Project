12a765f88a923fdd6bc8e33641a824f0fd317fdb
Alikhan <jonasofan.gubaev@gmail.com>
2023-04-12 11:40:42 +0600

    components added: button

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -8,12 +8,14 @@ import { AppComponent } from './app.component';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { HeaderComponent } from './layouts/header/header.component';
 import { FooterComponent } from './layouts/footer/footer.component';
+import { ButtonComponent } from './components/button/button.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
+    ButtonComponent,
   ],
   imports: [
     BrowserModule,
