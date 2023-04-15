254d0ccb53ecce4b0581307271931b853dd3b309
Alikhan <jonasofan.gubaev@gmail.com>
2023-04-15 12:44:59 +0600

    app-routing edited

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { MainComponent } from './layouts/main/main.component';
 
 const routes: Routes = [
-  { path: 'home', component: MainComponent },
+  { path: '**', component: MainComponent },
 ];
 
 @NgModule({
