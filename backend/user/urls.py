1ed93a6309a3f1d936ed5d75eb169b97de619165
Alikhan <jonasofan.gubaev@gmail.com>
2023-04-23 13:42:32 +0600

    user sign-in/sign-up added

--- backend/user/urls.py
+++ backend/user/urls.py
@@ -1,5 +1,13 @@
 from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+from .views import UserSignUpAPIView
 
 urlpatterns = [
-    
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('sign-up/', UserSignUpAPIView.as_view(), name='user-sign-up'),
 ]
